20e9d8ee2a05764aafacbd6f3d6a5936e06aabc3
Grimpy101 <gorupgorazd@gmail.com>
2021-11-05 14:36:23 +0100

    Add entity Uporabnik

--- entitete/src/main/java/entitete/Uporabnik.java
+++ entitete/src/main/java/entitete/Uporabnik.java
@@ -1,18 +1,65 @@
 package entitete;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jdk.jfr.Name;
+import org.eclipse.persistence.annotations.PrimaryKey;
 
-@Entity
+import javax.persistence.*;
+
+@Entity(name = "uporabniki")
+@NamedQueries(value = {
+        @NamedQuery(name = "Uporabnik.getAll",
+                query = "SELECT u FROM uporabniki u"),
+        @NamedQuery(name = "Uporabnik.getById",
+                query = "SELECT u FROM uporabniki u WHERE u.idUporabnik = :id"),
+        @NamedQuery(name = "Uporabnik.update",
+                query = "UPDATE uporabniki SET uporabniskoIme = :uporabniskoIme," +
+                        "email = :email, kodiranoGeslo = :kodiranoGeslo WHERE idUporabnik = :id"),
+        @NamedQuery(name = "Uporabnik.delete", query = "DELETE FROM uporabniki WHERE idUporabnik = :id")
+        // TODO: getRezervacije in getOcene
+})
 public class Uporabnik {
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUporabnik;
+    private String uporabniskoIme;
+    private String email;
+    private String kodiranoGeslo;
 
-    public void setId(Long id) {
-        this.id = id;
+    public Integer getIdUporabnik() {
+        return idUporabnik;
     }
 
-    @Id
-    public Long getId() {
-        return id;
+    public void setIdUporabnik(Integer idUporabnik) {
+        this.idUporabnik = idUporabnik;
+    }
+
+    public String getUporabniskoIme() {
+        return uporabniskoIme;
+    }
+
+    public void setUporabniskoIme(String uporabniskoIme) {
+        this.uporabniskoIme = uporabniskoIme;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getKodiranoGeslo() {
+        return kodiranoGeslo;
+    }
+
+    public void setKodiranoGeslo(String kodiranoGeslo) {
+        this.kodiranoGeslo = kodiranoGeslo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Uporabnik %s (%s)",
+                this.uporabniskoIme, this.email);
     }
 }
