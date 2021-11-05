d377fbbb07d81243e0df74435a5163dfb6ab0765
LukaPecoraro <lp3489@student.uni-lj.si>
2021-11-05 15:42:57 +0100

    Add entity Ocena and Rezervacija

--- entitete/src/main/java/entitete/Ocena.java
+++ entitete/src/main/java/entitete/Ocena.java
@@ -1,18 +1,37 @@
 package entitete;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+
+@NamedQueries(value = {
+        @NamedQuery(name = "Ocena.getAll", query = "SELECT o FROM Ocena o"),
+        @NamedQuery(name = "Ocena.getById", query = "SELECT o FROM Ocena o WHERE o.idOcena = :id"),
+        @NamedQuery(name = "Ocena.getByPostaja", query = "SELECT o FROM Ocena o WHERE o.polnilnaPostaja = :polnilnaPostaja"),
+        @NamedQuery(name = "Ocena.getByUporabnik", query = "SELECT o FROM Ocena o WHERE o.uporabnik = :uporabnik"),
+        @NamedQuery(name = "Ocena.update", query = "UPDATE Ocena SET ocena = :ocena, komentar = :komentar WHERE idOcena = :id"),
+        @NamedQuery(name = "Ocena.delete", query = "DELETE FROM Ocena WHERE idOcena = :id")
+})
 
 @Entity
 public class Ocena {
-    private Long id;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     @Id
-    public Long getId() {
-        return id;
-    }
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idOcena;
+
+    @ManyToOne
+    @JoinColumn(name = "idUporabnik")
+    private Uporabnik uporabnik;
+
+    @ManyToOne
+    @JoinColumn(name = "idPostaja")
+    private PolnilnaPostaja polnilnaPostaja;
+
+    private Integer ocena;
+
+    private String komentar;
+
+
+
+
 }
--- entitete/src/main/java/entitete/Rezervacija.java
+++ entitete/src/main/java/entitete/Rezervacija.java
@@ -1,18 +1,40 @@
 package entitete;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.sql.Date;
+
+@NamedQueries(value = {
+        @NamedQuery(name = "Rezervacija.getAll", query = "SELECT r FROM Rezervacija r"),
+        @NamedQuery(name = "Rezervacija.getById", query = "SELECT r FROM Rezervacija r WHERE r.idRezervacija = :id"),
+        @NamedQuery(name = "Rezervacija.getByPostaja", query = "SELECT r FROM Rezervacija r WHERE r.polnilnaPostaja = :polnilnaPostaja"),
+        @NamedQuery(name = "Rezervacija.getByUporabnik", query = "SELECT r FROM Rezervacija r WHERE r.uporabnik = :uporabnik"),
+
+        @NamedQuery(name = "Rezervacija.delete", query = "DELETE FROM Rezervacija WHERE idRezervacija = :id")
+})
 
 @Entity
 public class Rezervacija {
-    private Long id;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     @Id
-    public Long getId() {
-        return id;
-    }
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idRezervacija;
+
+    @ManyToOne
+    @JoinColumn(name = "idUporabnik")
+    private Uporabnik uporabnik;
+
+    @ManyToOne
+    @JoinColumn(name = "idPostaja")
+    private PolnilnaPostaja polnilnaPostaja;
+
+
+    @Temporal(TemporalType.DATE)
+    private Date datumRezervacije;
+
+    @Temporal(TemporalType.TIME)
+    private Date uraZacetka;
+
+    @Temporal(TemporalType.TIME)
+    private Date uraKonca;
+
 }
