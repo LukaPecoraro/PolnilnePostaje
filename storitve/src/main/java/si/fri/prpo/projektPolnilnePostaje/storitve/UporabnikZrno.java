9f5e41ff5a3571d918313def3147f738e13eb7c8
LukaPecoraro <lp3489@student.uni-lj.si>
2021-11-06 18:56:32 +0100

    add UporabnikZrno

---
+++ storitve/src/main/java/si/fri/prpo/projektPolnilnePostaje/storitve/UporabnikZrno.java
@@ -0,0 +1,11 @@
+package si.fri.prpo.projektPolnilnePostaje.storitve;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UporabnikZrno {
+
+        // implementacija
+
+
+}
