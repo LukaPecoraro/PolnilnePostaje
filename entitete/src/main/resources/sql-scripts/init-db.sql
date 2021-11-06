ae0c407a58e115e3f50c3b815399c62ac13d99cc
Grimpy101 <gorupgorazd@gmail.com>
2021-11-06 20:02:15 +0100

    Add init-db.sql

---
+++ entitete/src/main/resources/sql-scripts/init-db.sql
@@ -0,0 +1,8 @@
+INSERT INTO uporabniki (uporabniskoIme, email, kodiranoGeslo) VALUES ('Joze', 'joze.gorisek@ruse.si', 'skrivno_geslo');
+INSERT INTO uporabniki (uporabniskoIme, email, kodiranoGeslo) VALUES ('Miha', 'mojster.miha@mail.si', 'skrivno_geslo');
+INSERT INTO uporabniki (uporabniskoIme, email, kodiranoGeslo) VALUES ('Neznanec', 'foo.bar@posta.si', 'skrivno_geslo');
+INSERT INTO uporabniki (uporabniskoIme, email, kodiranoGeslo) VALUES ('Peter', 'peter.klepec@hribi.si', 'skrivno_geslo');
+INSERT INTO uporabniki (uporabniskoIme, email, kodiranoGeslo) VALUES ('Martin', 'martin.krpan@trojica.sv', 'skrivno_geslo');
+
+INSERT INTO polnilnePostaje (lokacija, uraOdprtja, uraZaprtja, cenaPolnjenja, tipPrikljucka, hitrostPolnjenja) VALUES ('Bogu za hrbtom 1, Prekljev Laz', '8:00', '22:00', '20.0', '1', '100');
+INSERT INTO polnilnePostaje (lokacija, uraOdprtja, uraZaprtja, cenaPolnjenja, tipPrikljucka, hitrostPolnjenja) VALUES ('Sredi vsega 12, Ljubljana', '9:00', '23:00', '10.0', '2', '50');
