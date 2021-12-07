9e88afe98f2ac95adddebccaccd1966bc259e85f
Grimpy101 <gorupgorazd@gmail.com>
2021-12-07 13:51:33 +0100

    Add API documentation for Uporabniki

--- api/src/main/java/si/fri/prpo/projektPolnilnePostaje/api/v1/viri/UporabnikiViri.java
+++ api/src/main/java/si/fri/prpo/projektPolnilnePostaje/api/v1/viri/UporabnikiViri.java
@@ -3,9 +3,12 @@ package si.fri.prpo.projektPolnilnePostaje.api.v1.viri;
 
 import com.kumuluz.ee.rest.beans.QueryParameters;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.headers.Header;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import si.fri.prpo.projektPolnilnePostaje.entitete.Uporabnik;
@@ -34,10 +37,13 @@ public class UporabnikiViri {
     private UporabnikZrno uporabnikZrno;
 
     @GET
-    @Operation(summary = "Pridobi uporabnike",
-            description = "Pridobi seznam uporabnikov z možnostjo paginacije in sortiranja.")
+    @Operation(summary = "Seznam uporabnikov",
+            description = "Vrne seznam uporabnikov")
     @APIResponses({
-            @APIResponse(description = "Seznam uporabnikov", responseCode = "200")
+            @APIResponse(description = "Seznam uporabnikov",
+                    responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = Uporabnik.class, type = SchemaType.ARRAY)),
+                    headers = @Header(name="X-Total-Count", description = "Število vrnjenih uporabnikov"))
     })
     public Response getUporabniki(){
         QueryParameters query = QueryParameters.query(uriInfo.getRequestUri().getQuery()).build();
@@ -51,16 +57,18 @@ public class UporabnikiViri {
     }
 
     @GET
-    @Operation(summary = "Pridobi določenega uporabnika",
-            description = "Pridobi izbranega uporabnika prek njegovega identifikatorja")
+    @Operation(summary = "Določeni uporabnik",
+            description = "Vrni izbranega uporabnika prek njegovega identifikatorja")
     @APIResponses({
             @APIResponse(description = "Uporabnik",
                     responseCode = "200",
-                    content = @Content(schema = @Schema(implementation = Uporabnik.class)),
-                    headers = {@Header(name = "X-Total-Count")})
+                    content = @Content(schema = @Schema(implementation = Uporabnik.class))
+            )
     })
     @Path("{idUporabnik}")
-    public Response getUporabnik(@PathParam("idUporabnik") Integer idUporabnik) {
+    public Response getUporabnik(@Parameter(
+            description = "ID uporabnika",
+            required = true) @PathParam("idUporabnik") Integer idUporabnik) {
         Uporabnik uporabnik = uporabnikZrno.getUporabnikById(idUporabnik);
         return uporabnik != null
                 ? Response.ok(uporabnik).build()
@@ -68,17 +76,41 @@ public class UporabnikiViri {
     }
 
     @POST
-    public Response addUporabnik(Uporabnik uporabnik) {
-        uporabnikZrno.createUporabnik(uporabnik);
-        return Response.noContent().build();
+    @Operation(summary = "Nov uporabnik",
+            description = "Dodaj novega uporabnika")
+    @APIResponses({
+            @APIResponse(responseCode = "201",
+                    description = "Uporabnik uspešno dodan"),
+            @APIResponse(responseCode = "405",
+                    description = "Validacijska napaka")
+    })
+    public Response addUporabnik(@RequestBody(
+            description = "ODT objekt za novega uporabnika",
+            required = true,
+            content = @Content(schema = @Schema(implementation = Uporabnik.class)))
+                                             Uporabnik uporabnik) {
+        Uporabnik u = uporabnikZrno.createUporabnik(uporabnik);
+        return Response
+                .status(Response.Status.CREATED)
+                .entity(u)
+                .build();
     }
 
     @DELETE
+    @Operation(summary = "Izbris uporabnika",
+            description = "Izbriši izbranega uporabnika")
+    @APIResponses({
+            @APIResponse(responseCode = "200",
+                    description = "Uporabnik uspešno izbrisan"),
+            @APIResponse(responseCode = "405",
+                    description = "Validacijska napaka")
+    })
     @Path("{idUporabnik}")
-    public Response deleteUporabnik(@PathParam("idUporabnik") Integer idUporabnik) {
+    public Response deleteUporabnik(@Parameter(
+            description = "ID uporabnika",
+            required = true)
+                                        @PathParam("idUporabnik") Integer idUporabnik) {
         uporabnikZrno.deleteUporabnik(idUporabnik);
-        return Response.noContent().build();
+        return Response.status(Response.Status.OK).build();
     }
-
-
 }
